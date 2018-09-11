81e9ba705e812277490b49afe035105dbb9ce00d
jcrawford62 <43072323+jcrawford62@users.noreply.github.com>
2018-09-11 10:04:22 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,7 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6uUT7oc3R2zeSng6z8a9MFJSJgX5yxyC1nyewC2Iuvpp9nBrqxuQJYJ9VSu74iSkCTpkCVVgkbKeIyJTP4g3UHMr6YfOpcVU1fdM1jrlI1ggXzY2TDYl/LAMEwlwJ0xhs9Mdf6LGJG9B72gcTDWxv3im9oqTU5YnI65+pvgtFOTOs9aSrVNSjtQynTxIQA+DH1W6drM2jOeumNCu0bZk+MFZuldDQ7E1fAn9mYQeayUsPMOKnM/3zIb0x0WHYxaw0W9qRR8+W7VLUYxy2kMkUf4pkrtKdQ2wsfCd1NT9D1nNnxmqK/UoUHwCt5SuMvNpiCey0kMOh+zcVMdGmQ3xL
+',
 	}  
 }
